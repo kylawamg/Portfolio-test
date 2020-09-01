b00928a38d28f4bc0a9cc6437ff1b8d5fe7adc23
david <deivid.develop@gmail>
2020-09-01 23:09:07 +0200

    Added modules for menu-responsive

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 //Material
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatMenuModule } from '@angular/material/menu';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   imports:  [
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatMenuModule,
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatMenuModule,
   ]
 })
 
