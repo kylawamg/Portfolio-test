a6c68ee6cd76bef3c902d5bdc01243538f5f3f79
david <deivid.develop@gmail>
2020-09-01 17:43:46 +0200

    Added anglular/material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from './components/header/header.component'
+import { HeaderComponent } from './components/header/header.component';
 
+//Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { HeaderComponent } from './components/header/header.component'
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports:  [
     HeaderComponent,
