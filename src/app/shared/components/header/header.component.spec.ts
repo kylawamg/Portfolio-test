0bf0445027d8a36dfd6d59476386acf2c24c7811
david <deivid.develop@gmail>
2020-09-04 22:55:20 +0200

    Leaving tested header- component

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AppComponent } from 'src/app/app.component';
+import { MaterialModule } from '../../../material/material.module';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports:[MaterialModule],
+      declarations: [ HeaderComponent ],
+      providers: [AppComponent]
     })
     .compileComponents();
   }));
