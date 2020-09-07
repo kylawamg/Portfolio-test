68483b8cf27c68ae1c8dfa046764eea0f9dc39ab
david <deivid.develop@gmail>
2020-09-07 11:53:06 +0200

    Adding the routes of the new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import  { ProjectsModule } from './features/projects/projects.module'
 
 //Components
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+
+  {
+    path: 'projects',
+    loadChildren: () => import('./features/projects/projects.module').then(m => m.ProjectsModule),
+  },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
