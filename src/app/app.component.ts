3b8658a42a586b45310560b35412f0029fd7deaf
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-09-16 18:44:46 +0200

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,32 @@
 import { Component, HostBinding } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
-import { tsParticles } from "tsparticles";
+import { tsParticles } from 'tsparticles';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
 
   @HostBinding('class') componentCssClass: any;
 
-  constructor(public overlayContainer: OverlayContainer,)
-  {
-    tsParticles.loadJSON("tsparticles", "../assets/particles.json")
-    .then((container) => {
-      console.log("callback - tsparticles config loaded");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
+  constructor(public overlayContainer: OverlayContainer) {
+    tsParticles
+      .loadJSON('tsparticles', 'assets/particles.json')
+      .then((container) => {
+        console.log('callback - tsparticles config loaded');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   public onSetTheme(nameClass: string) {
     this.overlayContainer.getContainerElement().classList.add(nameClass);
     this.componentCssClass = nameClass;
   }
-
-
 }
